c436611daf9fd253d6843d9dc1b971ef26eb36c8
Vincent P. Dizon <workvpdizon@gmail.com>
2022-01-18 15:23:54 +0800

    update

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { CssBaseline } from "@mui/material";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
